cb0bd62629e4c8476f076392024aaf957f211b77
TRINADH <tmandalapu.blr@gmail.com>
2024-08-27 21:17:44 +0530

    array demo

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -2,8 +2,8 @@
 
 NAMES=("A" "B" "C")
 
-echo "First name is:: $NAME[0]"
-echo "Second name is:: $NAME[1]"
-echo "Third name is:: $NAME[2]"
+echo "First name is:: ${NAME[0]}"
+echo "Second name is:: ${NAME[1]}"
+echo "Third name is:: ${NAME[2]}"
 
-echo "all names are:: $NAME[@]"
+echo "all names are:: ${NAME[@]}"
