6af2e32ea62f1ccb2e00ec5dc7d942841b5c5bcf
TRINADH <tmandalapu.blr@gmail.com>
2024-08-27 21:09:29 +0530

    variables demo

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username"
+
+read username
+
+echo "username is: $username"
+echo "please enter your password"
+
+read password
+echo "password is : $password"
