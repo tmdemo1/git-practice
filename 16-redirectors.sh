616f9daebed94ce8dacdf28c0dddd1d79248056b
TRINADH <tmandalapu.blr@gmail.com>
2024-08-31 22:27:51 +0530

    conditions demo

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -4,8 +4,8 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
-
+mkdir -p $LOGS_FOLDER &>>$LOG_FILE
+ls -l &>>$LOG_FILE
 USER=$(id -u)
 
 #text colors
