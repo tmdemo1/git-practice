cc3559f216ca6e82bb56bf3e706be0e90709dc66
TRINADH <tmandalapu.blr@gmail.com>
2024-08-31 22:20:49 +0530

    conditions demo

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -4,6 +4,9 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USER=$(id -u)
 
 #text colors
 #31m -> Red
@@ -14,10 +17,6 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-
-USER=$(id -u)
-
-mkdir -p $LOGS_FOLDER
 
 CHECK_ROOT(){
     if [ $USER -ne 0 ]
