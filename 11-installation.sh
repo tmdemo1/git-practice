ac35c9442c55d95b13ae9b806bbbed1ca588d27c
TRINADH <tmandalapu.blr@gmail.com>
2024-08-31 19:48:06 +0530

    conditions demo

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,11 +5,37 @@ USER=$(id -u)
 if [ $USER -ne 0 ]
 then
     echo "Please login with SUDO"
-else
-    echo "User $USER started executing the script $0"
-    dnf list installed git
+    exit 1
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed. Going to install Git."
+    dnf install git -y
     if [ $? -ne 0 ]
     then
-        dnf install git -y
+        echo "Git installed successfully."
+    else
+        echo "Git installation is not success... please check"
+        exit 1
     fi
+else
+    echo "Git already installed. Nothing to do."
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "Mysql is not installed. Going to install Mysql."
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Mysql installed successfully."
+    else
+        echo "Mysql installation is not success... please check"
+        exit 1
+    fi
+else
+    echo "Mysql already installed. Nothing to do."
 fi
