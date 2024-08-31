29b1fb2dddcf9be5080e95d9eed1f02047c06fb1
TRINADH <tmandalapu.blr@gmail.com>
2024-08-31 19:40:31 +0530

    conditions demo

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,10 +7,9 @@ then
     echo "Please login with SUDO"
 else
     echo "User $USER started executing the script $0"
-fi
-if [ $? -ne 0 ]
-then
-    echo "Last command failed. Exit with 1."
-else
-    echo "Successfully executed script"
+    dnf list installed git
+    if [ $? -ne 0 ]
+    then
+        dnf install git -y
+    fi
 fi
