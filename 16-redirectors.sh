31fd01147527add31f190ca3ff77c3f77bc18736
TRINADH <tmandalapu.blr@gmail.com>
2024-08-31 22:06:00 +0530

    conditions demo

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/logs/shell-script"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
