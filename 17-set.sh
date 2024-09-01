6af896c588f4aededafc5243a5fb37544df29001
TRINADH <tmandalapu.blr@gmail.com>
2024-09-01 17:45:00 +0530

    mysql script

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#set -e # setting the automatic exit, if we get error, set -ex for debug
+set -ex # setting the automatic exit, if we get error, set -ex for debug
 echo "Hello World Success"
 echoo "Hello World Failure"
 echo "Hello World Success"
